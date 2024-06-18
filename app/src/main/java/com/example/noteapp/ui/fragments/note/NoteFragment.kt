665649fcbd5dd20382d6432832b7d586094080ae
TonnyStarkkk <aidar20031982@gmail.com>
2024-06-18 18:03:10 +0600

    onBoard показывается один раз

--- app/src/main/java/com/example/noteapp/ui/fragments/note/NoteFragment.kt
+++ app/src/main/java/com/example/noteapp/ui/fragments/note/NoteFragment.kt
@@ -5,15 +5,55 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.noteapp.R
+import com.example.noteapp.databinding.FragmentNoteBinding
+import com.example.noteapp.databinding.FragmentOnBoardBinding
+import com.example.noteapp.ui.adapters.NoteAdapter
+import com.example.noteapp.ui.data.extension.getBackStackData
+import com.example.noteapp.ui.data.models.NoteModel
 
 class NoteFragment : Fragment() {
+
+    private lateinit var binding: FragmentNoteBinding
+    private lateinit var noteAdapter: NoteAdapter
+    private var list: ArrayList<NoteModel> = ArrayList()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_note, container, false)
+    ): View {
+        binding = FragmentNoteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        noteAdapter = NoteAdapter()
+        initialize()
+        setupListeners()
+        getData()
+    }
+
+    private fun initialize() {
+        binding.rvNote.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = noteAdapter
+        }
+    }
+
+    private fun setupListeners() = with(binding) {
+        addBtn.setOnClickListener{
+            findNavController().navigate(R.id.action_noteFragment_to_detailNoteFragment)
+        }
+    }
+
+    private fun getData() {
+       getBackStackData<String>("key"){ data ->
+           val noteModel = NoteModel(data)
+           list.add(noteModel)
+           noteAdapter.submitList(list)
+       }
     }
 }
